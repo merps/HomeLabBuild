d6d99145ae733b522a3ee504ff242e91ee542a27
JLCode-tech <j.lucia@f5.com>
2020-07-06 17:53:12 +1000

    join update

--- proxmox/lab/terraform/modules/k8scluster/kubeadm-token.sh
+++ proxmox/lab/terraform/modules/k8scluster/kubeadm-token.sh
@@ -7,7 +7,7 @@ eval "$(jq -r '@sh "HOST=\(.host) KEY=\(.key)"')"
 
 # Fetch the join command
 CMD=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_rsa.pub \
-    root@10.10.17.65 kubeadm token create --print-join-command)
+    root@192.168.1.140 kubeadm token create --print-join-command)
 
 # Produce a JSON object containing the join command
 jq -n --arg command "$CMD" '{"command":$command}'
