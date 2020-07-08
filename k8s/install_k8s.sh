838db4da54631eebdea53c618c93ecafb1966efe
JLCode-tech <j.lucia@f5.com>
2020-07-08 15:39:17 +1000

    update

--- k8s/install_k8s.sh
+++ k8s/install_k8s.sh
@@ -29,20 +29,6 @@ kubectl create secret generic -n metallb-system memberlist --from-literal=secret
 kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/metallb/metallbconfigmap.yaml
 
 
-#--- Dashboard Install ---------------------------------------------------------------------------------------
-#Install Dashboard
-kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/dashboard/dashboard.yaml
-#Create Admin Access
-kubectl create -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/dashboard/dashboard.admin-user.yml -f dashboard.admin-user-role.yml
-#Get Token for Access
-kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token
-kubectl -n kubernetes-dashboard get services
-
-#--- Storage Longhorn ---------------------------------------------------------------------------------------
-kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/longhorn/001-longhorn.yaml
-kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/longhorn/002-storageclass.yaml
-kubectl -n longhorn-system get services
-
 #--- Portainer Install ---------------------------------------------------------------------------------------
 #Portainer Install
 kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/portainer/portainer.yaml
@@ -52,10 +38,26 @@ kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/mast
 kubectl -n portainer get services
 
 
+#--- Dashboard Install ---------------------------------------------------------------------------------------
+#Install Dashboard
+#kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/dashboard/dashboard.yaml
+#Create Admin Access
+#kubectl create -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/dashboard/dashboard.admin-user.yml -f dashboard.admin-user-role.yml
+#Get Token for Access
+#kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token
+#kubectl -n kubernetes-dashboard get services
+
+
+#--- Storage Longhorn ---------------------------------------------------------------------------------------
+kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/longhorn/001-longhorn.yaml
+kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/longhorn/002-storageclass.yaml
+kubectl -n longhorn-system get services
+
+
 #--- Hubble Install ---------------------------------------------------------------------------------------
 #Hubble Install
-kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/hubble/hubble.yaml
-kubectl -n kube-system get services
+#kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/hubble/hubble.yaml
+#kubectl -n kube-system get services
 
 #HELM Install
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
@@ -70,21 +72,11 @@ helm repo add influxdata https://helm.influxdata.com/
 #kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/prometheus-grafana/prometheus/005-statemetrics.yaml
 kubectl create namespace monitoring
 helm install prometheus stable/prometheus --namespace monitoring --set alertmanager.persistentVolume.storageClass="longhorn" --set server.persistentVolume.storageClass="longhorn"
-#
-#
+
+
 # ---- Grafana -----------
 #kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/prometheus-grafana/grafana/001-grafana.yaml
-Create grafana.yaml
-datasources:
-  datasources.yaml:
-    apiVersion: 1
-    datasources:
-    - name: Prometheus
-      type: prometheus
-      url: http://prometheus-server.monitoring.svc.cluster.local
-      access: proxy
-      isDefault: true
-helm install grafana stable/grafana --namespace monitoring --set persistence.storageClassName="longhorn" --set persistence.enabled=true --set adminPassword='Mongo!123' --values grafana.yaml --set service.type=LoadBalancer
+helm install grafana stable/grafana --namespace monitoring --set persistence.storageClassName="longhorn" --set persistence.enabled=true --set adminPassword='Mongo!123' --values https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/prometheus-grafana/grafana/grafana.yaml --set service.type=LoadBalancer
 ### ---- Check LB IP and Port allocated  ---------------------------------------------------
 kubectl -n monitoring get services
 
@@ -99,6 +91,10 @@ kubectl logs -f --namespace monitoring $(kubectl get pods --namespace monitoring
 kubectl apply -f efk-logging/elastic.yaml
 kubectl apply -f efk-logging/kibana.yaml
 kubectl apply -f efk-logging/fluentd.yaml
+
+# ELk Logging Stack -----------------------------------------------------------------------------
+kubectl create namespace elk-logging
+helm install elasticsearch --version 7.8.0 elastic/elasticsearch -n elk-logging 
 
 
 
--- proxmox/lab/terraform/k8sleader.tf
+++ proxmox/lab/terraform/k8sleader.tf
@@ -36,6 +36,7 @@ resource "proxmox_vm_qemu" "k8sleader" {
 
     provisioner "remote-exec" {
         inline = [
+            "sudo apt-get install open-iscsi -y",
             "kubeadm init --pod-network-cidr=10.30.0.0/16 --apiserver-advertise-address=${var.leader_ip}"
         ]
       
@@ -52,7 +53,9 @@ resource "proxmox_vm_qemu" "k8sleader" {
             "cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
             "chown $(id -u):$(id -g) $HOME/.kube/config",
             "kubectl taint nodes --all node-role.kubernetes.io/master-",
-            "kubectl create -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/cilium/cilium-custom.yaml",
+            #"kubectl create -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/cilium/cilium-custom.yaml",
+            "kubectl create -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/calico/calicooperator.yaml",
+            "kubectl create -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/calico/calico.yaml",
             "kubectl get pods --all-namespaces",
             "kubectl get nodes -o wide"
         ]
@@ -66,7 +69,6 @@ resource "proxmox_vm_qemu" "k8sleader" {
     }
     provisioner "remote-exec" {
         inline = [
-            "sudo apt-get install open-iscsi -y",
             "sudo chmod 600 /home/debian/.ssh/authorized_keys",
             "sudo cp /home/debian/.ssh/authorized_keys /root/.ssh/authorized_keys"
         ]
