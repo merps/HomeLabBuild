092f414014f799a1c7e3121283c7412ddac4fdcc
JLCode-tech <j.lucia@f5.com>
2020-07-14 17:29:37 +1000

    updates

--- k8s/install_k8s.sh
+++ k8s/install_k8s.sh
@@ -88,9 +88,10 @@ kubectl apply -f efk-logging/fluentd.yaml
 # ELk Logging Stack -----------------------------------------------------------------------------
 # ElasticSearch Install
 kubectl create namespace elk-logging
-helm install elasticsearch --version 7.8.0 elastic/elasticsearch -n elk-logging --set minimumMasterNodes="1" --values https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/efk-logging/elastic_values.yaml
+helm repo add elastic https://helm.elastic.co
+helm install elasticsearch --version 7.8.0 elastic/elasticsearch -n elk-logging --set minimumMasterNodes="1" --set replicas="2" --values https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/efk-logging/elastic_values.yaml
 # Logstash Install
-helm install logstash elastic/logstash -n elk-logging --set replicas="3" --values https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/efk-logging/logstash_values.yaml
+helm install logstash elastic/logstash -n elk-logging --set replicas="2" --values https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/efk-logging/logstash_values.yaml
 #Kibana
 #helm install kibana elastic/kibana -n elk-logging --set replicas="3" --set imageTag="7.7.1" --set elasticsearchHosts="http://elasticsearch-master.elk-logging:9200" --values https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/efk-logging/kibana_values.yaml
 kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/efk-logging/kibana.yaml
