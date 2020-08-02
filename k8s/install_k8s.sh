303e61b20efbe898ff173b1e482211be6e8e204d
JLCode-tech <j.lucia@f5.com>
2020-08-02 17:45:16 +1000

    updates

--- k8s/install_k8s.sh
+++ k8s/install_k8s.sh
@@ -14,41 +14,22 @@
 #Verify pods start up correctly
 #kubectl -n kube-system get pods --watch
 
-sudo apt-get install     apt-transport-https     ca-certificates     curl     gnupg-agent     software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-cat /etc/docker/daemon.json 
-systemctl enable docker && sudo systemctl start docker
-systemctl enable kubelet && sudo systemctl start kubelet
-bash -c "echo "1" > /proc/sys/net/bridge/bridge-nf-call-iptables"
-bash -c "echo "1" > /proc/sys/net/bridge/bridge-nf-call-ip6tables"
-sysctl --system
-sudo swapoff -a
-
-#Docker Install - https://docs.docker.com/engine/install/debian/
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo docker run hello-world
-
-
 #--- Worker Node Install ---------------------------------------------------------------------------------------
 #Install Worker Nodes
 #sudo kubeadm join 192.168.1.140:6443 --token m5tfd1.48ca3j74b7wv2ht4 --discovery-token-ca-cert-hash sha256:fad842c01a621d27fb7a02932c641260cb069a67afab656f85eff23dffc72caf
+
+#--- Load Balancer Install ---------------------------------------------------------------------------------------
+#Install LB - Using Metallb
+kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/metallb/metallb-namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/metallb/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/metallb/metallbconfigmap.yaml
 
 #HELM Install - https://helm.sh/docs/intro/install/
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-
-#--- Load Balancer Install ---------------------------------------------------------------------------------------
-#Install LB - Using Metallb
-kubectl --kubeconfig ~/.kube/config_minikube apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/metallb/metallb-namespace.yaml
-kubectl --kubeconfig ~/.kube/config_minikubeapply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/metallb/metallb.yaml
-# On first install only
-kubectl --kubeconfig ~/.kube/config_minikube create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/metallb/metallbconfigmap.yaml
-
 
 #HELM Repos to Add Install
 helm repo add portainer http://portainer.github.io/portainer-k8s
@@ -65,7 +46,7 @@ helm repo add billimek https://billimek.com/billimek-charts/
 ### ---- Check LB IP and Port allocated  ---------------------------------------------------
 #kubectl -n portainer get services
 kubectl create namespace portainer
-helm --kubeconfig ~/.kube/config_minikube install portainer portainer/portainer-beta --namespace portainer --set service.type="LoadBalancer"
+helm install portainer portainer/portainer-beta --namespace portainer --set service.type="LoadBalancer"
 
 #--- Storage Longhorn ---------------------------------------------------------------------------------------
 kubectl apply -f https://raw.githubusercontent.com/JLCode-tech/HomeLabBuild/master/k8s/longhorn/001-longhorn.yaml
@@ -101,7 +82,7 @@ kubectl -n monitoring get services
 helm install influx influxdata/influxdb --namespace monitoring --set persistence.enabled=true,persistence.size=10Gi --set persistence.storageClass="longhorn"
 
 # ---- Speedtest--------
-helm install speedtest billimek/speedtest -n monitoring --set config.influxdb.host="influx-influxdb.monitoring" --set config.delay="300" --set debug="true"
+helm install speedtest billimek/speedtest -n monitoring --set config.influxdb.host="influx-influxdb.monitoring" --set config.delay="1800" --set debug="true"
 kubectl logs -f --namespace monitoring $(kubectl get pods --namespace monitoring -l app=speedtest -o jsonpath='{ .items[0].metadata.name }')
 
 # EFK monitoring stack --------------------------------------------------------------------------
